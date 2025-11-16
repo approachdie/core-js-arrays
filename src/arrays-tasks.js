3df630ed91d34036d6c2f473ecdbdd077726d1c0
Дмитрий Шергин <dmitrishergin@MacBook-Air-Dmitrij.local>
2025-11-16 22:49:38 +0300

    feat: add next

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -602,8 +602,8 @@ function shiftArray(arr, n) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  return arr.sort();
 }
 
 /**
