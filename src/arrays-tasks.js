452c53d6361a45cf4131622865a2737c7632c873
Дмитрий Шергин <dmitrishergin@MacBook-Air-Dmitrij.local>
2025-11-16 22:13:42 +0300

    feat: add next

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -380,8 +380,12 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const result = [];
+  for (let i = 0; i < len; i += 1) {
+    result.push(2 * i + 1);
+  }
+  return result;
 }
 
 /**
