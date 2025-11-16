2a3cc5d89a7427dbdbf659fa51bab4ffd4ebf817
Дмитрий Шергин <dmitrishergin@MacBook-Air-Dmitrij.local>
2025-11-16 20:33:11 +0300

    feat: add next

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -360,8 +360,12 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const result = [];
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    result.push(arr.slice(i, i + chunkSize));
+  }
+  return result;
 }
 
 /**
