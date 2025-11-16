f02dcc6a77a4c388552b9e6979a3e055d2bc7d76
Дмитрий Шергин <dmitrishergin@MacBook-Air-Dmitrij.local>
2025-11-16 22:47:54 +0300

    feat: add next

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -575,8 +575,18 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const arrLength = arr.length;
+  let N = n;
+
+  if (arrLength === 0 || n === 0) return arr;
+
+  N %= arrLength;
+
+  if (N < 0) {
+    N += arrLength;
+  }
+  return arr.slice(arrLength - N).concat(arr.slice(0, arrLength - N));
 }
 
 /**
